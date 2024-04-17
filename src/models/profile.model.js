b484712fc726c7ae4f52a84d44ffc8b0ac861e81
Emperor <jainchist@gmail.com>
2024-04-17 15:24:01 +0530

    created profile schema conains additional details of user

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -1,2 +1,17 @@
 const mongoose = require("mongoose")
 
+const profileSchema = new mongoose.Schema({
+
+    college:{
+        type:String,
+    },
+    location:{
+        type:String,
+    },
+    maxStreak:{
+        type:Number,
+    },
+    
+})
+
+module.exports = mongoose.model("Profile", profileSchema);
